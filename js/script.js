685794f236811394b2d69e2289c03f1987dca7ad
corawym <corawym@gmail.com>
2017-07-17 11:54:46 -0700

    added smooth scrolling in the navigation bar

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
+// jQuery 
 $(document).ready(function(){
   
+  // shop carousel style
   $('.carousel').flickity({
     prevNextButtons: false,
     initialIndex: 0,
     wraparound: false,
     autoPlay: true,
-    cellAlign: 'left',
+    cellAlign: "left",
     contain: true
   });
 
+  // header style - smooth scrolling
+  var offset = $(".header-fixed").height();
 
+  $(window).resize(function() {
+    offset = $(".header-fixed").height();
+  });
 
-
-
+  $(".main-nav a[href*=\\#]").on("click", function(event){  
+    event.preventDefault();
+    $("html,body").animate({scrollTop:$(this.hash).offset().top-offset}, 500);
+  });
+  
+  
 
 
 });
-
 
