5ecedd1cfefb4da3fc9e4d394d333e7b42772406
corawym <corawym@gmail.com>
2017-07-17 14:41:13 -0700

    added email validation

--- js/script.js
+++ js/script.js
@@ -20,11 +20,24 @@ $(document).ready(function(){
 
   $(".main-nav a[href*=\\#]").on("click", function(event){  
     event.preventDefault();
-    $("html,body").animate({scrollTop:$(this.hash).offset().top-offset}, 500);
+    $("html,body,article").animate({scrollTop:$(this.hash).offset().top-offset}, 500);
   });
   
   // updates style
-
+  $("#btn-sub").on("click", function(event) {
+    if (ValidateEmail($("#email-input").val())) {
+      event.preventDefault();
+      alert( "Thanks for subscribing!" );
+      $("#email-input").val("");
+    } 
+  });
 
 });
 
+function ValidateEmail(mail){
+  if (/^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/.test(mail)){
+    return (true);
+  }else{
+    return (false);
+  }
+}
